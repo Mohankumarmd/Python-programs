018808b954cdae661757ce075a6c1e7029ad0c91
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-03 23:22:36 +0530

    Added files

--- 01.py
+++ 01.py
@@ -1,1 +1,29 @@
 print("Hello world")
+
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = a
+
+if b > largest:
+    largest = b
+if c > largest:
+    largest = c
+
+print("Largest number is:", largest)
+
+
+
+string = input("Enter a string: ")
+reverse = string[::-1]
+print("Reversed string:", reverse)
