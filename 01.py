95982f8749f1ad98c3bcfe1d2cc868d6a3fd7e1b
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-04 21:46:20 +0530

    Added files

--- 01.py
+++ 01.py
@@ -27,3 +27,35 @@ print("Largest number is:", largest)
 string = input("Enter a string: ")
 reverse = string[::-1]
 print("Reversed string:", reverse)
+
+num = 1234
+reverse = 0
+
+while num != 0:
+    reverse = reverse * 10 + num % 10
+    num = num // 10
+
+print("Reversed Number:", reverse)
+
+
+num = 121
+original = num
+reverse = 0
+
+while num != 0:
+    reverse = reverse * 10 + num % 10
+    num //= 10
+
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+num = 5
+fact = 1
+
+for i in range(1, num + 1):
+    fact *= i
+
+print("Factorial:", fact)
