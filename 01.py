f309e86daab287f6f207fd6ef2e2f94f8f0c29d4
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-07 09:30:50 +0530

    Add

--- 01.py
+++ 01.py
@@ -121,3 +121,14 @@ else:
 
 num = int(input("Enter number: "))
 print("Square:", num*num)
+
+
+text = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+count = 0
+
+for char in text:
+    if char in vowels:
+        count += 1
+
+print("Number of vowels:", count)
