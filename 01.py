e53f8ed14ce5a2cf68b0c9f2b18eedbd04a7786b
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-09 23:18:11 +0530

    Add

--- 01.py
+++ 01.py
@@ -181,3 +181,11 @@ else:
 for i in range(1,21):
     if i % 2 == 0:
         print(i)
+
+
+year = int(input("Enter year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap Year")
+else:
+    print("Not Leap Year")
