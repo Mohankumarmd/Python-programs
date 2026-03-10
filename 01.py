bee001c930af9ddba85b60cc193e832bc4694882
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-10 18:29:04 +0530

    Add

--- 01.py
+++ 01.py
@@ -196,3 +196,18 @@ c = 25
 f = (c * 9/5) + 32
 
 print("Fahrenheit:", f)
+
+
+num = int(input("Enter number: "))
+temp = num
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum += digit ** 3
+    num //= 10
+
+if sum == temp:
+    print("Armstrong")
+else:
+    print("Not Armstrong")
