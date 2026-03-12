5df100ec78c0fb26888be2ee2113f3948aa61ff3
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-12 23:35:37 +0530

    Add

--- 01.py
+++ 01.py
@@ -275,3 +275,14 @@ while low <= high:
     if arr[mid] == key:
         print("Found")
         break
+
+arr = [64,25,12,22]
+
+for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1,len(arr)):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print(arr)
