a33cc305b4fb5aefb0f73b29d455274b3b3f87a7
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-24 22:40:53 +0530

    Add

--- 01.py
+++ 01.py
@@ -379,3 +379,10 @@ for i in range(1, 6):
     for j in range(i):
         print("*", end=" ")
     print()
+
+
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
