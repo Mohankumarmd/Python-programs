16bf2d252d7dd59443f9989b71cda1d68ab515c8
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-26 22:59:52 +0530

    Add

--- 01.py
+++ 01.py
@@ -386,3 +386,12 @@ for i in range(n):
     for j in range(n):
         print("*", end=" ")
     print()
+
+num = int(input("Enter number: "))
+count = 0
+
+while num > 0:
+    num //= 10
+    count += 1
+
+print("Digits =", count)
