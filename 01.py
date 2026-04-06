df512da9facf01d1c98389d630351d7537cb0e97
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-04-06 21:58:13 +0530

    Add

--- 01.py
+++ 01.py
@@ -412,3 +412,15 @@ for i in range(n, 0, -1):
     for j in range(i):
         print("*", end="")
     print()
+
+
+n = 5
+
+for i in range(1, n + 1):
+    # spaces
+    for j in range(n - i):
+        print(" ", end="")
+    # stars
+    for k in range(2 * i - 1):
+        print("*", end="")
+    print()
