da63c113f208773a5672e4eb498702d3fc63f11b
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-21 11:48:46 +0530

    Add

--- 01.py
+++ 01.py
@@ -881,3 +881,13 @@ for i in range(n):
     for j in range(n):
         print("*", end=" ")
     print()
+
+
+
+num = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num + 1):
+    fact *= i
+
+print("Factorial is:", fact)
