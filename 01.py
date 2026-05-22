a8344e9c094532c03b9ef09cfbe45431a3dd3d68
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-22 11:20:41 +0530

    Add

--- 01.py
+++ 01.py
@@ -891,3 +891,11 @@ for i in range(1, num + 1):
     fact *= i
 
 print("Factorial is:", fact)
+
+
+n = 5
+
+# for i in range(1, n + 1):
+#     for j in range(i):
+#         print("*", end="")
+#     print()
